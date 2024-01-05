5909878f2334f634b5f6a0644a4d29e168e93926
Victor Duran <victor.duran.em@gmail.com>
2024-01-05 17:40:59 -0500

    Add Telemetry

--- src/main/java/com/example/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/example/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,9 @@ package com.example.currencyconversionservice.controller;
 
 import com.example.currencyconversionservice.CurrencyExchangeProxy;
 import com.example.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,13 +15,24 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Objects;
 
+@Configuration
+class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
 
     private final CurrencyExchangeProxy proxy;
+    private final RestTemplate restTemplate;
 
-    public CurrencyConversionController(CurrencyExchangeProxy proxy) {
+    public CurrencyConversionController(CurrencyExchangeProxy proxy, RestTemplate restTemplate) {
         this.proxy = proxy;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
@@ -32,7 +46,7 @@ public class CurrencyConversionController {
         uriVariables.put("from", from);
         uriVariables.put("to", to);
 
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity(
                 "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
                 CurrencyConversion.class,
                 uriVariables
